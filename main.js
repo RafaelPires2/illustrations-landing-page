6d0a7ce3a88498d1aabbef796533f5050d439792
RafaelPires2 <rafaelbrpires@gmail.com>
2023-02-17 11:20:11 -0300

    feat: responsive for all devices

--- main.js
+++ main.js
@@ -3,25 +3,25 @@ const cardTwo = document.querySelector(".card-two");
 const cardThree = document.querySelector(".card-three");
 
 function changeCardOne() {
-  cardOne.style.width = "388px";
+  cardOne.style.width = "50%";
   document.documentElement.style.setProperty("--default-color", "#FFD280");
 
-  cardTwo.style.width = "183px";
-  cardThree.style.width = "183px";
+  cardTwo.style.width = "25%";
+  cardThree.style.width = "25%";
 }
 
 function changeCardTwo() {
-  cardTwo.style.width = "388px";
+  cardTwo.style.width = "50%";
   document.documentElement.style.setProperty("--default-color", "#9BA0FC");
 
-  cardOne.style.width = "183px";
-  cardThree.style.width = "183px";
+  cardOne.style.width = "25%";
+  cardThree.style.width = "25%";
 }
 
 function changeCardThree() {
-  cardThree.style.width = "388px";
+  cardThree.style.width = "50%";
   document.documentElement.style.setProperty("--default-color", "#8FC7FF");
 
-  cardTwo.style.width = "183px";
-  cardOne.style.width = "183px";
+  cardTwo.style.width = "25%";
+  cardOne.style.width = "25%";
 }
